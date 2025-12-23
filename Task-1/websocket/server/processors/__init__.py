6b91e87696c59c01440bce5b6f2c7a0cc922fee6
Test User <testuser.carl@gmail.com>
2025-12-23 12:02:38 +0500

    Delete Task-1/websocket directory

--- Task-1/websocket/server/processors/__init__.py
+++
@@ -1,6 +0,0 @@
-from .soniox_stt import SonioxSTTService
-from .cartesia_tts import CartesiaTTSService
-from .simple_response import SimpleResponseService
-from .groq_llm import GroqLLMService
-
-__all__ = ["SonioxSTTService", "CartesiaTTSService", "SimpleResponseService", "GroqLLMService"]
